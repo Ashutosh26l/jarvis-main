54b2805846f0cf52ebe6e7a452e0767eb254d4de
ashutosh26l <aksmba2611@gmail.com>
2025-03-24 10:44:25 +0530

    .

--- jarvis_main.py
+++ jarvis_main.py
@@ -5,14 +5,20 @@ from pydub.playback import play
 from pydub import AudioSegment
 import speech_recognition as sr
 import pyttsx3
-from speak import speak
-import subprocess
-
+import jarvis_command
+engine=pyttsx3.init("sapi5")
+voices=engine.getProperty("voices")
+engine.setProperty("voice",voices[0].id)
+rate=engine.getProperty('rate')
+engine.setProperty("rate",rate-30)
+def speak(text):
+    print("Jarvis:",text)
+    engine.say(text)
+    engine.runAndWait()
 
 porcupine=None
 pyaud=None
 audio_stream=None
-p=None
 def startsound():
     audio=AudioSegment.from_wav("start up sound.wav")
     play(audio)
@@ -21,7 +27,7 @@ def endsound():
     audio=AudioSegment.from_wav("end up sound.wav")
     play(audio)
 try:
-    porcupine=pvporcupine.create(keywords=["jarvis","alexa","ok google"])
+    porcupine=pvporcupine.create(keywords=["jarvis","alexa"])
     paud=pyaudio.PyAudio()
     audio_stream=paud.open(rate=porcupine.sample_rate,channels=1,format=pyaudio.paInt16,input=True,frames_per_buffer=porcupine.frame_length)
     while True:
@@ -29,20 +35,14 @@ try:
         keyword=struct.unpack_from("h"*porcupine.frame_length,keyword)
         keyword_index=porcupine.process(keyword)
         if keyword_index>=0:
-            if p!=None:
-                p.terminate()
             startsound()
             recognize=sr.Recognizer()
             with sr.Microphone() as source:
-                audio=recognize.listen(source,4,3)
+                audio=recognize.listen(source,3,3)
                 endsound()
             try:
                 query=recognize.recognize_google(audio,language='en-in')
-                file=open("myfile.txt",'w')
-                file.write(query)
-                file.close()
-                print(query)
-                p=subprocess.Popen("jarvis_command.exe")
+                jarvis_command.command(query)
             except sr.UnknownValueError:
                 speak("not recognize")
             
